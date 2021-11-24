8093d42d2ac99c8b1711337ce376193abb4bf8d1
hrvojekolar <hrvojekolar1998@gmail.com>
2021-11-24 15:12:25 +0100

    Add input pins define in Pins

--- src/Pins.hpp
+++ src/Pins.hpp
@@ -7,4 +7,21 @@
 #define rst -1
 #define dio0 18
 
+#define BTN_0 0
+#define BTN_8 1
+#define BTN_5 2
+#define BTN_2 3
+#define BTN_1 4
+#define BTN_4 5
+#define BTN_7 6
+#define BTN_L 7
+#define BTN_BACK 8
+#define BTN_ENTER 9
+#define BTN_DOWN 10
+#define BTN_UP 11
+#define BTN_3 12
+#define BTN_6 13
+#define BTN_9 14
+#define BTN_R 15
+
 #endif //CHATTER_LIBRARY_PINS_H
