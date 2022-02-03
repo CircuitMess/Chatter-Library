a0dd90ca9da9acc5dc6f90b788525e33c51d873d
hrvojekolar <hrvojekolar1998@gmail.com>
2022-02-03 09:50:56 +0100

    Set default values in Settings

--- src/Settings.h
+++ src/Settings.h
@@ -6,9 +6,9 @@
 const uint8_t ShutdownMinutes[] = {0, 5, 10, 15, 20};
 
 struct SettingsData {
-	bool sound = false;
-	uint8_t sleepTime = 1; //index of value in vector, values : {0, 1, 5, 15, 30}[min]
-	uint32_t screenBrightness = 255; // speed * (speedMultiplier / 255)
+	bool sound = true;
+	uint8_t sleepTime = 1; //index of value in vector, values : {0, 5, 10, 15, 20}[min]
+	uint32_t screenBrightness = 255;
 
 };
 
