94078d986a1f27d817ce16e1e8a1a06fba42a320
Filip Budiša <filip@circuitmess.com>
2022-09-16 14:45:51 +0200

    Adjust shutdown options
    (cherry picked from commit 2c98280a74a5835fe513555593b8dff4d4058d1c)

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -6,8 +6,8 @@ SettingsImpl Settings;
 const uint32_t SleepSeconds[SLEEP_STEPS] = { 0, 30, 60, 2 * 60, 5 * 60 };
 const char* SleepText[SLEEP_STEPS] = { "OFF", "30 sec", "1 min", "2 min", "5 min" };
 
-const uint32_t ShutdownSeconds[SHUTDOWN_STEPS] = { 0, 30, 60, 2 * 60, 5 * 60 };
-const char* ShutdownText[SHUTDOWN_STEPS] = { "OFF", "30 sec", "1 min", "2 min", "5 min" };
+const uint32_t ShutdownSeconds[SHUTDOWN_STEPS] = { 0, 60*60, 6 * 60*60, 24 * 60*60, 2 * 24 * 60*60 };
+const char* ShutdownText[SHUTDOWN_STEPS] = { "OFF", "1 hour", "6 hours", "1 day", "2 days" };
 
 bool SettingsImpl::begin(){
 	return Settings::init(&data, sizeof(SettingsData));
