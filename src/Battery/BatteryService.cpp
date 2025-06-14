a84c4d8a39a735458263cc1720b00a4180171b3b
Filip Budiša <filip@circuitmess.com>
2025-06-14 22:57:20 +0200

    HW v2 changes * Adjust battery calibration and mapping for v2 * v2.0 display panel setup * HW revision v2.0 test code

--- src/Battery/BatteryService.cpp
+++ src/Battery/BatteryService.cpp
@@ -1,15 +1,24 @@
 #include "BatteryService.h"
 #include <Loop/LoopManager.h>
 #include <soc/efuse_reg.h>
+#include <Util/HWRevision.h>
 #include "../Pins.hpp"
 
 BatteryService Battery;
 
-const uint16_t BatteryService::MeasureInterval = 10; //in seconds
-const uint16_t BatteryService::MeasureCount = 10; //in seconds
 
 uint16_t BatteryService::mapReading(uint16_t reading){
-	return map(reading, 720, 920, 3700, 4500);
+	if(HWRevision::get() == 1){
+		// At 3600 reading 3600, at 4600 reading 4850
+		/*const int volt1 = reading * 4;
+		const int volt2 = std::round(((float) (volt1 - 3600) / 1250.0f) * 250.0f);
+		return volt1 - volt2;*/
+
+		const int volt1 = reading * 4;
+		return map(volt1, 3780, 5030, 3600, 4600);
+	}else{
+		return map(reading, 720, 920, 3700, 4500);
+	}
 }
 
 void BatteryService::loop(uint micros){
@@ -20,7 +29,7 @@ void BatteryService::loop(uint micros){
 		measureCounter++;
 		if(measureCounter == MeasureCount){
 			measureSum = measureSum / MeasureCount;
-			voltage = mapReading(measureSum);
+			voltage = measureSum;
 			measureCounter = 0;
 			measureSum = 0;
 		}
@@ -30,11 +39,20 @@ void BatteryService::loop(uint micros){
 void BatteryService::begin(){
 	LoopManager::addListener(this);
 	pinMode(BATTERY_PIN, INPUT);
+
+	if(HWRevision::get() == 1){
+		// TODO: Check if this stays low during deep sleep
+		pinMode(CALIB_EN, OUTPUT);
+		digitalWrite(CALIB_EN, 0);
+
+		calibrate();
+	}
+
 	for(int i = 0; i < 10; i++){
 		measureSum += analogRead(BATTERY_PIN);
 	}
 	measureSum = measureSum / MeasureCount;
-	voltage = mapReading(measureSum);
+	voltage = measureSum;
 	measureSum = 0;
 }
 
@@ -67,11 +85,34 @@ uint8_t BatteryService::getPercentage() const{
 }
 
 uint16_t BatteryService::getVoltage() const{
-	return voltage + getVoltOffset();
+	if(HWRevision::get() == 1){
+		return mapReading(voltage + getVoltOffset());
+	}else{
+		return mapReading(voltage) + getVoltOffset();
+	}
 }
 
-int16_t BatteryService::getVoltOffset(){
-	uint32_t upper = REG_GET_FIELD(EFUSE_BLK3_RDATA3_REG, EFUSE_RD_ADC1_TP_HIGH);
-	uint32_t lower = REG_GET_FIELD(EFUSE_BLK3_RDATA3_REG, EFUSE_RD_ADC1_TP_LOW);
-	return (upper << 7) | lower;
+int16_t BatteryService::getVoltOffset() const{
+	if(HWRevision::get() == 1){
+		return calibOffset;
+	}else{
+		uint32_t upper = REG_GET_FIELD(EFUSE_BLK3_RDATA3_REG, EFUSE_RD_ADC1_TP_HIGH);
+		uint32_t lower = REG_GET_FIELD(EFUSE_BLK3_RDATA3_REG, EFUSE_RD_ADC1_TP_LOW);
+		return (upper << 7) | lower;
+	}
+}
+
+void BatteryService::calibrate(){
+	digitalWrite(CALIB_EN, 1);
+	delay(100);
+
+	float sum = 0;
+	for(int i = 0; i < MeasureCount; i++){
+		sum += analogRead(CALIB_READ);
+	}
+	const uint16_t volt = std::round(sum / (float) MeasureCount);
+
+	calibOffset = CalibRef - volt;
+
+	digitalWrite(CALIB_EN, 0);
 }
--- src/Battery/BatteryService.h
+++ src/Battery/BatteryService.h
@@ -14,18 +14,23 @@ public:
 	uint8_t getPercentage() const;
 	uint16_t getVoltage() const;
 
-	static uint16_t mapReading(uint16_t reading);
-
-	static int16_t getVoltOffset();
+	int16_t getVoltOffset() const;
 
 private:
 	uint16_t voltage = 0; //in mV
-	static const uint16_t MeasureInterval;
-	static const uint16_t MeasureCount;
+	static constexpr uint16_t MeasureInterval = 10; // [s]
+	static constexpr uint16_t MeasureCount = 10; // [s]
 	uint measureMicros = 0;
 	float measureSum = 0;
 	uint8_t measureCounter = 0;
 
+	static uint16_t mapReading(uint16_t reading);
+
+	//For HW revision 2.0
+	void calibrate();
+
+	static constexpr uint16_t CalibRef = 624;
+	int16_t calibOffset;
 };
 
 extern BatteryService Battery;
